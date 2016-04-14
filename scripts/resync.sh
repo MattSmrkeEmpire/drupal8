30dd597edb013e1d36446bbcf3395a9eaf134763
Matt Smrke <matthew.smrke@empire.ca>
2016-04-14 15:44:44 -0400

    strips out resync

--- scripts/resync.sh
+++ scripts/resync.sh
@@ -7,9 +7,6 @@
 site_name='Development'
 db_name='drupal8db'
 
-# Drop any existing database so site-install doesn't choke on foreign key constraints.
-#ssh vagrant@drupal8.local 'mysql -u root -proot -e "DROP DATABASE IF EXISTS drupal8db"'
-
 # Install Drupal virgin version
 echo Site name will be ${site_name}
-drush @d8 site-install standard -y --account-name=dev --account-pass=dev --db-url=mysql://root:root@localhost/${db_name} --site-name=${site_name}
+drush site-install standard -y --account-name=dev --account-pass=dev --db-url=mysql://root:root@localhost/${db_name} --site-name=${site_name}
